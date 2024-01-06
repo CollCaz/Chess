5fbf8505df7c6f629a398a298c8b351eea2b0e9a
CollCaz <62307305+CollCaz@users.noreply.github.com>
2024-01-06 22:25:16 +0200

    created sql migrations for the players table

---
+++ migrations/000001_create_players_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS players (
+  id bigserial PRIMARY KEY,
+  created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+  name text NOT NULL,
+  world_rank integer NOT NULL,
+  birth_year integer NOT NULL,
+  federation text NOT NULL,
+  sex text NOT NULL,
+  fide_id integer NOT NULL,
+  fide_title text NOT NULL,
+  standard_rating integer NOT NULL,
+  rapid_rating integer NOT NULL,
+  blitz_rating integer NOT NULL
+);
